5d204b6f6d8552afe9b71cca68451132b131cbc3
Noah Fechtor-Pradines <nfechtorpradines@college.harvard.edu>
2015-07-03 05:09:37 -0400

    up to 60

--- p14.py
+++ p14.py
@@ -1,4 +1,13 @@
+# Hi Timmeh :3
+
+
+# I only put 'largest' in my code because I was curious
+# how big the numbers might get before going back to 1
+
 largest = 1
+
+# 'collatz' just checks even/odd then performs the appropriate 
+# Collatz operation. You can ignore the 'largest' line.
 
 def collatz(x):
     global largest
@@ -8,7 +17,18 @@ def collatz(x):
         largest = max(largest,3*x+1)
         return 3*x+1
 
+
+# 'arr' stores the number of steps required to get from i to 
+# 1 in 'arr[i]'. I'll just define 0 and 1 to take (0) moves.
+
 arr = [0,0]
+
+# 'check' essentially iterates 'collatz' on x, but there's a heuristic
+# I assume we are checking numbers starting from 2 and going up. This way
+# if I call 'check(i,i)', if I ever get get to a value less than 'i', I 
+# know that I've already calculated how many steps the rest of the time 
+# takes, so just add on that value. This actually makes computation time
+# exponentially faster.
 
 def check(x,v):
     global arr
@@ -20,8 +40,12 @@ def check(x,v):
 
 cap = 1000000
 
+# fill in all values of our array
+
 for i in range(2,cap):
     arr.append(check(i,i))
+
+# now find the value with the maximum number and report its array position
 
 mx = 0
 val = 1
