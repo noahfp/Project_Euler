4686097eb4f4a43068c9e01bed2bdfe8d43ce351
Noah Fechtor-Pradines <nfechtorpradines@college.harvard.edu>
2020-08-14 16:32:16 -0400

    jupyter?

--- p086.py
+++ p086.py
@@ -1,15 +1,21 @@
 import math
 import numpy as np 
 
+def gcd(x,y):
+	x,y = max(x,y),min(x,y)
+	while y > 0:
+		x, y = y, x % y
+	return x
+
 def triples(limit):
 	ans = []
 	for m in xrange(1,int(math.floor(math.sqrt(2*limit+1)))+1):
 		for n in xrange(1, m):
-			if (n + m) % 2 == 1:
+			if gcd(n,m) == 1 and (n % 2)*(m % 2) == 0:
 				a, b = max(m**2 - n**2, 2*m*n), min(m**2 - n**2, 2*m*n)
 				for i in xrange(1, min(limit*2//a,limit//b)+1):
 					ans.append([a*i,b*i])
-	return ans
+	return sorted(ans)
 
 def cubes(a,b,limit):
 	ans = 0
@@ -19,15 +25,16 @@ def cubes(a,b,limit):
 		M = b
 		m = a-a//2
 		ans += max(0, M-m+1)
-	print a,b,limit,ans
+	# print a,b,limit,ans
 	return ans
 
 def check(limit):
 	return sum([cubes(x[0],x[1],limit) for x in triples(limit)])
 
 # UGH FIGURE THIS SHIT OUT
-limit = 10
+limit = 1818
 
-print triples(limit)
+# print triples(limit)
 print check(limit)
+
 
